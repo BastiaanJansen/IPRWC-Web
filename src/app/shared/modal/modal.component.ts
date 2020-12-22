880a35fa76866932216b82fe17f8639d423cf892
Bastiaan Jansen <Bastiaanj7@outlook.com>
2020-12-22 13:37:38 +0100

    Shared components

---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+
+@Component({
+	selector: "app-modal",
+	templateUrl: "./modal.component.html",
+	styleUrls: ["./modal.component.scss"],
+})
+export class ModalComponent implements OnInit {
+	@Input() title: string;
+	@Input() subtitle: string;
+	@Input() showBody: boolean = true;
+	@Input() showFooter: boolean = true;
+	@Output() closeModal = new EventEmitter<null>();
+
+	icons = { faTimesCircle };
+
+	constructor() {}
+
+	ngOnInit(): void {}
+
+	close(): void {
+		this.closeModal.emit();
+	}
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HeaderComponent } from "./components/header/header.component";
+import { ModalComponent } from "./modal/modal.component";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { DropdownComponent } from './dropdown/dropdown.component';
+import { DropdownSelectedDirective } from './dropdown/dropdown-selected.directive';
+import { DropdownOptionDirective } from './dropdown/dropdown-option.directive';
 
 @NgModule({
-	declarations: [HeaderComponent],
-	imports: [CommonModule],
+	declarations: [HeaderComponent, ModalComponent, DropdownComponent, DropdownSelectedDirective, DropdownOptionDirective],
+	imports: [CommonModule, FontAwesomeModule],
 	exports: [HeaderComponent],
 })
 export class SharedModule {}
